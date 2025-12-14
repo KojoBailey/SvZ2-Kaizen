b9923b82cc1d63a732dfcd1fa163b2154298fc41
Kojo Bailey <kojomolojo@gmail.com>
2025-12-14 00:14:52 +0000

    Add more WaveCommandSchema fields

--- Assets/Scripts/Assembly-CSharp/WaveCommandSchema.cs
+++ Assets/Scripts/Assembly-CSharp/WaveCommandSchema.cs
@@ -4,10 +4,41 @@ public class WaveCommandSchema
 	[DataBundleKey]
 	public int index;
 
+	public enum Type
+	{
+		Spawn,
+		Delay,
+	};
+	[DataBundleField(ColumnWidth = 200)]
+	public Type type;
+
 	[DataBundleSchemaFilter(typeof(EnemySchema), false)]
 	[DataBundleField(ColumnWidth = 200)]
 	[DataBundleRecordTableFilter("Enemies")]
 	public DataBundleRecordKey enemy;
+
+	[DataBundleField(ColumnWidth = 200)]
+	public int count;
+
+	public enum Spacing
+	{
+		Loose,
+		Moderate,
+		Tight,
+	};
+	[DataBundleField(ColumnWidth = 200)]
+	public Spacing spacing;
+
+	public enum StartMode
+	{
+		After,
+		Overlap,
+	};
+	[DataBundleField(ColumnWidth = 200)]
+	public StartMode startMode;
+
+	[DataBundleField(ColumnWidth = 200)]
+	public float advanceAt;
 
 	[DataBundleField(ColumnWidth = 200)]
 	public string command;
